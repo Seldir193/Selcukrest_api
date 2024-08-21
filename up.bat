e42d892901b5764939cdb48090fb192299e49f10
Sel <selcuk.80@outlook.de>
2024-08-21 10:59:56 +0200

    first working version

--- up.bat
+++ up.bat
@@ -1,4 +1,4 @@
 git add .
 git commit -m "%*"
 git push
-chrome "https://www.pythonanywhere.com/user/Seldir/consoles/35251600/"
+Start-Process "https://www.pythonanywhere.com/user/Seldir/consoles/35251600/"
